f39a8a410ada05059189bf2b8a39e14650b33c1c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-07 15:13:15 +0000

    Deploy yamresh/react-ci to github.com/yamresh/react-ci.git:gh-pages

---
+++ precache-manifest.9ea9dfcdfcf9107e701611f51d31245e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0075bea737609813662ec15d0f781dbd",
+    "url": "/react-ci/index.html"
+  },
+  {
+    "revision": "e2064b008f1d0f32e908",
+    "url": "/react-ci/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7181933175022bbb120f",
+    "url": "/react-ci/static/js/2.9a5348c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-ci/static/js/2.9a5348c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2064b008f1d0f32e908",
+    "url": "/react-ci/static/js/main.8b5611ce.chunk.js"
+  },
+  {
+    "revision": "c4e23d421d28408368c9",
+    "url": "/react-ci/static/js/runtime-main.8598bbc7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-ci/static/media/logo.5d5d9eef.svg"
+  }
+]);
